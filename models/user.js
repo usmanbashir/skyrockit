710a7507dc59e68f7d7de776d051740283948265
Usman Bashir <me@usmanbashir.com>
2024-10-24 20:38:11 +0300

    Add Application Schema to User Model

--- models/user.js
+++ models/user.js
@@ -1,14 +1,20 @@
 const mongoose = require('mongoose');
 
+const applicationSchema = new mongoose.Schema({
+  company:      { type: String, required: true },
+  title:        { type: String, required: true },
+  notes:        { type: String },
+  postingLink:  { type: String },
+  status: {
+    type: String,
+    enum: ['interested', 'applied', 'interviewing', 'rejected', 'accepted'],
+  }
+});
+
 const userSchema = mongoose.Schema({
-  username: {
-    type: String,
-    required: true,
-  },
-  password: {
-    type: String,
-    required: true,
-  },
+  username: { type: String, required: true },
+  password: { type: String, required: true },
+  applications: [applicationSchema],
 });
 
 const User = mongoose.model('User', userSchema);
